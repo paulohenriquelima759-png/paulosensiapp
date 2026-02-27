8be5ca844380f48b77ffa88f1a1f9ec464272761
paulohenriquelima759-png <paulohenriquelima759@gmail.com>
2026-02-27 17:20:43 -0400

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,1 +1,9 @@
-ok
+fetch("https://paulosensi-server-1.onrender.com/api/ping")
+.then(r=>r.json())
+.then(d=>{
+document.getElementById("status").innerText="Servidor ONLINE ✅";
+})
+
+function entrar(){
+alert("Bem vindo DONO 🔥")
+}
